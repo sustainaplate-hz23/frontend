711295a9dcd85c08cfdcf20a15b5a716c6356a59
cslylla <81328857+cslylla@users.noreply.github.com>
2023-09-17 01:43:12 +0200

    Add subscription options

--- src/components/SubscriptionModal.jsx
+++ src/components/SubscriptionModal.jsx
@@ -1,5 +1,6 @@
-import {Modal, Box, Stack, Popover} from "@mui/material";
+import {Modal, Box, Button, Grid, Typography} from "@mui/material";
 import migrosbox from "../assets/Migros-box.png";
+import { theme } from "../utils/theme";
 
 const style = {
     position: 'absolute',
@@ -7,7 +8,6 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 900,
-    height: 'calc(100% - 150px)',
     bgcolor: 'background.paper',
     border: '1px solid #f4f4f4',
     borderRadius: '10px',
@@ -22,15 +22,61 @@ const SubscriptionModal = ({openSub, handleClose}) => {
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
+            sx={{ overflow: "scroll"}}
         >
 
         <Box sx={style}>
-            <Stack>
-                <h1>
-                    Subscription Options
-                </h1>
-            <img src={migrosbox} alt="Migros Subscription Box" height="50vh" />
-            </Stack>
+            <Grid container alignItems="center" flexDirection="column">
+                <Grid item textAlign="center">
+                    <Typography variant="h1">
+                        Subscription Options
+                    </Typography>
+                </Grid>
+                <Grid item xs={6} md={6}>
+                     <img src={migrosbox} alt="Migros Subscription Box" width="100%" margin-left="auto" margin-right="auto"/>
+                </Grid>
+                    <Grid item container gap={2} flexDirection="column" alignItems="center">
+                    <Grid item container justifyContent="space-between" md={6} sx={{border: `2px solid ${theme.palette.primary.main}`, padding: "1rem", borderRadius : 2}}>
+                        <Grid item>
+                            <Typography variant="p">1 week</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="p">5.90/month</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h2">CHF 5.90</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid item container justifyContent="space-between" md={6} sx={{border: `2px solid ${theme.palette.grey[700]}`, padding: "1rem", borderRadius : 2}}>
+                        <Grid item>
+                            <Typography variant="p">2 weeks</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="p">5.00/week</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h2">CHF 10.00</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid item container justifyContent="space-between" md={6} sx={{border: `2px solid ${theme.palette.grey[700]}`, padding: "1rem", borderRadius : 2}}>
+                        <Grid item>
+                            <Typography variant="p">4 weeks</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="p">4.00/month</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h2">CHF 16.00</Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Button variant="contained" sx={{
+                    color: 'white', marginTop: "1rem" }}>Subscribe to delivery pass</Button>
+                </Grid>
+                
+            </Grid>
+            
 
         </Box>
         </Modal>
